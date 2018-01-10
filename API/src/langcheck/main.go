a93bfd039b46477127ca299b0c8c5a07ef8f8c8d
Eric Lindgren <nephilim.eric@gmail.com>
2018-01-10 11:27:19 -0800

    added language test for haskell (currently failing)

--- API/src/langcheck/main.go
+++ API/src/langcheck/main.go
@@ -10,8 +10,8 @@ func main() {
 	box := sandbox.New("data/compilers.json")
 	compilerTests := make(map[string]string)
 
-	// need test for Haskell
 	// currently passing:
+
 	// compilerTests["C++"] = "#include <iostream>\nusing namespace std;\n\nint main() {\n\tcout<<\"Hello\";\n\treturn 0;\n}"
 	// compilerTests["Java"] = "\n\nimport java.io.*;\n\nclass myCode\n{\n\tpublic static void main (String[] args) throws java.lang.Exception\n\t{\n\t\t\n\t\tSystem.out.println(\"Hello\");\n\t}\n}"
 	// compilerTests["C#"] = "using System;\n\npublic class Challenge\n{\n\tpublic static void Main()\n\t{\n\t\t\tConsole.WriteLine(\"Hello\");\n\t}\n}"
@@ -25,10 +25,15 @@ func main() {
 	// compilerTests["PHP"] = "<?php\n$ho = fopen('php://stdout', \"w\");\n\nfwrite($ho, \"Hello\");\n\n\nfclose($ho);\n"
 
 	// currently broken:
-	// FIXME
+
+	// Haskell ghc missing, maybe need to rebuild docker file
+	// compilerTests["Haskell"] = "module Main where\nmain = putStrLn \"Hello\""
+
+	// Scala: don't understand the error this generates
 	// compilerTests["Scala"] = "object HelloWorld {def main(args: Array[String]) = println(\"Hello\")}"
-	// FIXME compiler
-	compilerTests["Rust"] = "fn main() {\n\tprintln!(\"Hello\");\n}"
+
+	// Rust seems to be missing and there's a problem setting environment variables
+	// compilerTests["Rust"] = "fn main() {\n\tprintln!(\"Hello\");\n}"
 
 	/*
 		"MySQL":"create table myTable(name varchar(10));\ninsert into myTable values(\"Hello\");\nselect * from myTable;",
